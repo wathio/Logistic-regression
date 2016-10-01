bc1467c66eb1a98134f3e980d3300ebcd8ca27a1
Thierry Wambo <jean.oscar@yahoo.fr>
2016-10-01 09:21:34 -0500

    Update ex2_reg.m

--- ex2_reg.m
+++ ex2_reg.m
@@ -1,21 +1,5 @@
-%% Machine Learning Online Class - Exercise 2: Logistic Regression
-%
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the second part
-%  of the exercise which covers regularization with logistic regression.
-%
-%  You will need to complete the following functions in this exericse:
-%
-%     sigmoid.m
-%     costFunction.m
-%     predict.m
-%     costFunctionReg.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
+%========Part1: Regualrization======================
+%  This file contains code to run regularization with logistic regression
 
 %% Initialization
 clear ; close all; clc
@@ -40,21 +24,7 @@ ylabel('Microchip Test 2')
 legend('y = 1', 'y = 0')
 hold off;
 
-
-%% =========== Part 1: Regularized Logistic Regression ============
-%  In this part, you are given a dataset with data points that are not
-%  linearly separable. However, you would still like to use logistic 
-%  regression to classify the data points. 
-%
-%  To do so, you introduce more features to use -- in particular, you add
-%  polynomial features to our data matrix (similar to polynomial
-%  regression).
-%
-
 % Add Polynomial Features
-
-% Note that mapFeature also adds a column of ones for us, so the intercept
-% term is handled
 X = mapFeature(X(:,1), X(:,2));
 
 % Initialize fitting parameters
@@ -72,16 +42,8 @@ fprintf('Cost at initial theta (zeros): %f\n', cost);
 fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
-%% ============= Part 2: Regularization and Accuracies =============
-%  Optional Exercise:
-%  In this part, you will get to try different values of lambda and 
-%  see how regularization affects the decision coundart
-%
-%  Try the following values of lambda (0, 1, 10, 100).
-%
-%  How does the decision boundary change when you vary lambda? How does
-%  the training set accuracy vary?
-%
+%% ============= Part 2:Regularization and Accuracies =============
+
 
 % Initialize fitting parameters
 initial_theta = zeros(size(X, 2), 1);
