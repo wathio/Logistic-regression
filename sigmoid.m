18b8bbdc92f2c1901257df9baca26104032036ee
Thierry Wambo <jean.oscar@yahoo.fr>
2016-10-01 10:11:31 -0500

    Update sigmoid.m

--- sigmoid.m
+++ sigmoid.m
@@ -2,11 +2,11 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
+%initializing values
 g = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+% ===========================================================
+% Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
 [m,n]=size(z)
